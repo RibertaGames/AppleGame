e94b3aa8e01b0a058605282f4c282b8c70d6653e
SyoukiYano <RibertaGames@gmail.com>
2024-05-27 16:03:09 +0900

    ATT(AppTrackingTransparency)の実装

---
+++ Assets/Editor/ATTPostProcessBuild.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_IOS
+
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+//20221206ATTのFrameとinfo.plistを自動で追加するスクリプト
+
+public static class ATTPostProcessBuild
+{
+    [PostProcessBuild]//ビルド時に実行する
+    private static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+    {
+        string infoPlistPath = buildPath + "/Info.plist";
+        PlistDocument infoPlist = new PlistDocument();
+        infoPlist.ReadFromFile(infoPlistPath);
+        PlistElementDict root = infoPlist.root;
+        root.SetString("NSUserTrackingUsageDescription", "あなたの好みに合わせた広告を表示するために使用されます");
+        infoPlist.WriteToFile(infoPlistPath);
+
+        string pbxProjectPath = PBXProject.GetPBXProjectPath(buildPath);
+        PBXProject pbxProject = new PBXProject();
+        pbxProject.ReadFromFile(pbxProjectPath);
+        string targetGuid = pbxProject.GetUnityFrameworkTargetGuid();
+        pbxProject.AddFrameworkToProject(targetGuid, "AppTrackingTransparency.framework", true);
+        pbxProject.WriteToFile(pbxProjectPath);
+    }
+}
+
+#endif
